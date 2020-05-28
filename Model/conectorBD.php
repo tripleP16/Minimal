888d5f0e732cd976b3cf2c2b013dac909b9dac73
Pablo Perez <perez51160900@gmail.com>
2020-05-28 12:37:22 -0400

    creacion de los archivos y parte de la base de datos

---
+++ Model/conectorBD.php
@@ -0,0 +1,81 @@
+<?php 
+    require('user.php');
+    date_default_timezone_set('America/Caracas');
+class conectorBD{
+    private $host;
+    private $user;
+    private $password;
+    private $conexion;
+
+    function __construct($host, $user, $password){
+      $this->host = $host;
+      $this->user = $user;
+      $this->password = $password;
+    }
+
+    function ejecutarQuery($query){
+        return $this->conexion->query($query);
+      }
+
+    function initConexion($nombre_db){
+      $this->conexion = new mysqli($this->host, $this->user, $this->password, $nombre_db);
+      if ($this->conexion->connect_error) {
+        return "Error:" . $this->conexion->connect_error;
+      }else {
+        return "OK";
+      }
+    }
+
+    function insertData($tabla, $data){
+        $sql = 'INSERT INTO '.$tabla.' (';
+        $i = 1;
+        foreach ($data as $key => $value) {
+          $sql .= $key;
+          if ($i<count($data)) {
+            $sql .= ', ';
+          }else $sql .= ')';
+          $i++;
+        }
+        $sql .= ' VALUES (';
+        $i = 1;
+        foreach ($data as $key => $value) {
+          $sql .= $value;
+          if ($i<count($data)) {
+            $sql .= ', ';
+          }else $sql .= ');';
+          $i++;
+        }
+        return $this->ejecutarQuery($sql);
+      }
+  
+      function consultar($tablas, $campos, $condicion = ""){
+        $sql = "SELECT ";
+        $result = array_keys($campos);
+        $ultima_key = end($result);
+        foreach ($campos as $key => $value) {
+          $sql .= $value;
+          if ($key!=$ultima_key) {
+            $sql.=", ";
+          }else $sql .=" FROM ";
+        }
+    
+        $result = array_keys($tablas);
+        $ultima_key = end($result);
+        foreach ($tablas as $key => $value) {
+          $sql .= $value;
+          if ($key!=$ultima_key) {
+            $sql.=", ";
+          }else $sql .= " ";
+        }
+    
+        if ($condicion == "") {
+          $sql .= ";";
+        }else {
+          $sql .= $condicion.";";
+        }
+        return $this->ejecutarQuery($sql);
+      }
+  
+
+}
+?>
